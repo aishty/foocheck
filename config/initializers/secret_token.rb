db37a6b43a0a99c34aa8dfd5491df9927fe25814
Aayish <aayishshetty@gmail.com>
2014-01-27 23:22:11 +0530

    init repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foocheck::Application.config.secret_token = '9952ff69c00b28e9b9cdb2d878311dd86fd3fc4e5bb7767e503472e460a74ef1835fea69ef732a9d79d258cbdc0770ae1aac020f147009b0a76717ef36c01486'
